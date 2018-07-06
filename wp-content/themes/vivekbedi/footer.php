09aa6642df3a51f502905183e244fd2130593496
debbiechiang <971618+debbiechiang@users.noreply.github.com>
2018-07-06 02:23:10 -0400

    Initial commits.

---
+++ wp-content/themes/vivekbedi/footer.php
@@ -0,0 +1,20 @@
+   <footer class="blog-footer">
+     <p class="down-1">Copyright &reg; 2018 Vivek Bedi</p>
+     <ul class="social-links">
+       <li>
+         <a href="#" target="_blank">Facebook</a>
+         <a href="#" target="_blank">Twitter</a>
+         <a href="#" target="_blank">Instagram</a>
+         <a href="#" target="_blank">LinkedIn</a>
+         <a href="#" target="_blank">Google Plus</a>
+       </li>
+     </ul>
+   </footer>
+
+   <!-- Bootstrap core JavaScript
+   ================================================== -->
+   <!-- Placed at the end of the document so the pages load faster -->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+ </body>
+</html>
---
+++ wp-content/themes/vivekbedi/index.php
@@ -0,0 +1,102 @@
+<?php wp_header(); ?>
+
+   <div class="container">
+     <section class="hero homepage">
+       <h1 class="blog-title">Hi, I'm Vivek Bedi.</h1>
+       <p class="lead blog-description">I am a passionate product leader who is obsessed with user experience.</p>
+     </section>
+     <div class="row">
+       <section class="col-sm-8 blog-main">
+         <h2 class="body-title">Latest News</h2>
+         <a class="eyebrow down-1" href="">See All News</a>
+         <article class="tile tile--dark-bg">
+           <a href="" class="tile__category down-1">Blog</a>
+           <span class="tile__date down-1">April 2, 2018</span>
+
+           <h3 class="tile__title"><a href="">The User-Obsessed PM</a></h3>
+
+           <div class="tile__tags">
+             <ul>
+               <li class="tile__tag">
+                 <a href="" class="tile__tag-link down-1">Lectures</a>
+               </li>
+               <li class="tile__tag">
+                 <a href="" class="tile__tag-link down-1">Career</a>
+               </li>
+               <li class="tile__tag">
+                 <a href="" class="tile__tag-link down-1">Talks</a>
+               </li>
+               <li class="tile__tag">
+                 <a href="" class="tile__tag-link down-1">Product</a>
+               </li>
+             </ul>
+           </div>
+         </article>
+         <article class="tile tile--grey-bg">
+           <a href="" class="tile-type down-1">Blog</a>
+           <span class="tile down-1">April 2, 2018</span>
+
+           <h3><a href="" class="tile__title">The User-Obsessed PM</a></h3>
+
+           <div class="tile__tags">
+             <ul>
+               <li class="tile__tag">
+                 <a href="" class="tile__tag-link down-1">Lectures</a>
+               </li>
+               <li class="tile__tag">
+                 <a href="" class="tile__tag-link down-1">Career</a>
+               </li>
+               <li class="tile__tag">
+                 <a href="" class="tile__tag-link down-1">Talks</a>
+               </li>
+               <li class="tile__tag">
+                 <a href="" class="tile__tag-link down-1">Product</a>
+               </li>
+             </ul>
+           </div>
+         </article>
+
+         <article class="tile tile__2x tile--grey-bg">
+           <h3 class="tile__title"><a href="">Important Information</a></h3>
+           <p class="tile__blurb">I had a great time discussing the importance of bridging the gap between user needs and building amazing experiences.</p>
+           <a href="" class="tile__read-more down-1">Read More</a>
+         </article>
+
+         <nav>
+           <ul class="pager">
+             <li><a href="#">Previous</a></li>
+             <li><a href="#">Next</a></li>
+           </ul>
+         </nav>
+
+       </section><!-- /.blog-main -->
+
+       <aside class="col-sm-3 col-sm-offset-1 blog-sidebar">
+         <div class="sidebar-module sidebar-module-inset">
+           <h4>Speaking Engagements</h4>
+           <ol class="">
+             <li class="speaker-series">
+               <img src="" class="speaker-series__img" />
+               <a href="">Marquette University Speaker Series</a>
+             </li>
+             <li class="speaker-series">
+               <img src="" class="speaker-series__img" />
+               <a href="">Marquette University Speaker Series</a>
+             </li>
+             <li class="speaker-series">
+               <img src="" class="speaker-series__img" />
+               <a href="">Marquette University Speaker Series</a>
+             </li>
+             <li class="speaker-series">
+               <img src="" class="speaker-series__img" />
+               <a href="">Marquette University Speaker Series</a>
+             </li>
+           </ol>
+           <a href="" class="sidebar-link">See All Speaking Engagements.</a>
+         </div>
+       </div><!-- /.blog-sidebar -->
+
+     </div><!-- /.row -->
+
+   </div><!-- /.container -->
+   <?php wp_footer(); ?>
