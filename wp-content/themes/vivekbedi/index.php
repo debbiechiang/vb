e8a4c93191c3beb85dc0a7faca16e931c52d4b06
debbiechiang <971618+debbiechiang@users.noreply.github.com>
2018-07-07 00:32:25 -0400

    Styling homepage, mostly tiles.

--- wp-content/themes/vivekbedi/index.php
+++ wp-content/themes/vivekbedi/index.php
@@ -1,73 +1,111 @@
 <?php get_header(); ?>
- <div class="wrapper container-fluid">
-     <section class="hero homepage row">
-       <h1 class="hero__title">Hi, I'm Vivek Bedi. I am a passionate product leader who is obsessed with user experience.</p>
-     </section>
-     <div class="row">
-       <section class="col-sm-8 blog-main">
-         <h2 class="body-title">Latest News</h2>
-         <a class="eyebrow down-1" href="">See All News </a>
-         <article class="tile tile--dark-bg">
-           <a href="" class="tile__category down-1">Blog</a>
-           <span class="tile__date down-1">April 2, 2018</span>
+ <div class="wrapper">
+  <section class="hero homepage row">
+    <h1 class="hero__title">Hi, I'm Vivek Bedi. I am a passionate product leader who is obsessed with user experience.</p>
+  </section>
+</div>
+<div class="wrapper container-fluid"
+  <div class="row">
+    <main class="col-sm-8 main">
+      <div class="module-header row">
+        <h2 class="module-title col-sm-8">Latest News</h2>
+        <a class="module-link down-1 col-sm-4" href="">See All News</a>
+      </div>
+      <section class="tile-container">
+        <article class="tile tile__1x tile--dark-bg">
+          <div class="tile__meta">
+            <a href="" class="tile__category down-1">Blog</a>
+            <span class="tile__date down-1">April 2, 2018</span>
+          </div>
+          <h3 class="tile__title"><a href="">The User-Obsessed PM</a></h3>
 
-           <h3 class="tile__title"><a href="">The User-Obsessed PM</a></h3>
+          <div class="tile__tags">
+            <ul>
+              <li class="tile__tag">
+                <a href="" class="tile__tag-link down-1">Lectures</a>
+              </li>
+              <li class="tile__tag">
+                <a href="" class="tile__tag-link down-1">Career</a>
+              </li>
+              <li class="tile__tag">
+                <a href="" class="tile__tag-link down-1">Talks</a>
+              </li>
+              <li class="tile__tag">
+                <a href="" class="tile__tag-link down-1">Product</a>
+              </li>
+            </ul>
+          </div>
+        </article>
+        <article class="tile tile__1x tile--grey-bg">
+          <div class="tile__meta">
+            <a href="" class="tile__category down-1">Blog</a>
+            <span class="tile__date down-1">April 2, 2018</span>
+          </div>
+          <h3 class="tile__title"><a href="">The User-Obsessed PM</a></h3>
 
-           <div class="tile__tags">
-             <ul>
-               <li class="tile__tag">
-                 <a href="" class="tile__tag-link down-1">Lectures</a>
-               </li>
-               <li class="tile__tag">
-                 <a href="" class="tile__tag-link down-1">Career</a>
-               </li>
-               <li class="tile__tag">
-                 <a href="" class="tile__tag-link down-1">Talks</a>
-               </li>
-               <li class="tile__tag">
-                 <a href="" class="tile__tag-link down-1">Product</a>
-               </li>
-             </ul>
-           </div>
-         </article>
-         <article class="tile tile--grey-bg">
-           <a href="" class="tile-type down-1">Blog</a>
-           <span class="tile__date down-1">April 2, 2018</span>
-
-           <h3><a href="" class="tile__title">The User-Obsessed PM</a></h3>
-
-           <div class="tile__tags">
-             <ul>
-               <li class="tile__tag">
-                 <a href="" class="tile__tag-link down-1">Lectures</a>
-               </li>
-               <li class="tile__tag">
-                 <a href="" class="tile__tag-link down-1">Career</a>
-               </li>
-               <li class="tile__tag">
-                 <a href="" class="tile__tag-link down-1">Talks</a>
-               </li>
-               <li class="tile__tag">
-                 <a href="" class="tile__tag-link down-1">Product</a>
-               </li>
-             </ul>
-           </div>
-         </article>
+          <div class="tile__tags">
+            <ul>
+              <li class="tile__tag">
+                <a href="" class="tile__tag-link down-1">Lectures</a>
+              </li>
+              <li class="tile__tag">
+                <a href="" class="tile__tag-link down-1">Career</a>
+              </li>
+              <li class="tile__tag">
+                <a href="" class="tile__tag-link down-1">Talks</a>
+              </li>
+              <li class="tile__tag">
+                <a href="" class="tile__tag-link down-1">Product</a>
+              </li>
+            </ul>
+          </div>
+        </article>
 
          <article class="tile tile__2x tile--grey-bg">
            <h3 class="tile__title"><a href="">Important Information</a></h3>
-           <p class="tile__blurb">I had a great time discussing the importance of bridging the gap between user needs and building amazing experiences.</p>
+           <p class="tile__blurb">I had a great time discussing the importance of bridging the gap between user needs and building amazing experiences. I had a great time discussing the importance of bridging the gap between user needs and building amazing experiences.</p>
            <a href="" class="tile__read-more down-1">Read More</a>
          </article>
 
-         <nav>
-           <ul class="pager">
-             <li><a href="#">Previous</a></li>
-             <li><a href="#">Next</a></li>
-           </ul>
-         </nav>
+         <article class="tile tile__1x tile--dark-bg">
+           <div class="tile__meta">
+             <a href="" class="tile__category down-1">Blog</a>
+             <span class="tile__date down-1">April 2, 2018</span>
+           </div>
+           <h3 class="tile__title"><a href="">In the News: Product Management Today Feature Article</a></h3>
 
-       </section><!-- /.blog-main -->
+           <div class="tile__tags">
+             <ul>
+               <li class="tile__tag">
+                 <a href="" class="tile__tag-link down-1">Career</a>
+               </li>
+               <li class="tile__tag">
+                 <a href="" class="tile__tag-link down-1">Product</a>
+               </li>
+             </ul>
+           </div>
+         </article>
+         <article class="tile tile__1x tile--grey-bg">
+           <div class="tile__meta">
+             <a href="" class="tile__category down-1">Blog</a>
+             <span class="tile__date down-1">April 2, 2018</span>
+           </div>
+           <h3 class="tile__title"><a href="">Product Management Today Feature Article</a></h3>
+
+           <div class="tile__tags">
+             <ul>
+               <li class="tile__tag">
+                 <a href="" class="tile__tag-link down-1">Career</a>
+               </li>
+               <li class="tile__tag">
+                 <a href="" class="tile__tag-link down-1">Product</a>
+               </li>
+             </ul>
+           </div>
+         </article>
+       </section>
+
+       </main><!-- /.blog-main -->
 
        <aside class="col-sm-3 col-sm-offset-1 blog-sidebar">
          <div class="sidebar-module sidebar-module-inset">
