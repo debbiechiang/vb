d00c66ea9f8e8e1678311d2fa3fa1cf2b54df155
debbiechiang <971618+debbiechiang@users.noreply.github.com>
2018-07-07 15:34:54 -0400

    News Page 3x tiles.

--- wp-content/themes/vivekbedi/index.php
+++ wp-content/themes/vivekbedi/index.php
@@ -4,27 +4,35 @@
     <h1 class="hero__title"><?php echo get_bloginfo( 'description' ); ?></p>
   </section>
 </div>
-<div class="wrapper container-fluid">
+<main class="wrapper container-fluid">
   <div class="row">
-    <main class="col-sm-8 main">
+    <div class="col-sm-8 main">
       <div class="module-header row">
         <h2 class="module-title col-xs-8">Latest News</h2>
         <a class="module-link down-1 col-xs-4" href="">See All News</a>
       </div>
-      <section class="tile-container">
+    </div>
+  </div>
+
+  <div class="row">
+    <section class="tile-container col-sm-8 tile-2up">
+      <div class="row">
         <?php hm_get_template_part('tile', ['bg' => 'dark-bg', 'width' => '1x']) ?>
         <?php hm_get_template_part('tile', ['bg' => 'grey-bg', 'width' => '1x']) ?>
+      </div>
+      <div class="row">
         <?php hm_get_template_part('tile', ['bg' => 'grey-bg', 'width' => '2x']) ?>
+      </div>
+      <div class="row">
         <?php hm_get_template_part('tile', ['bg' => 'dark-bg', 'width' => '1x']) ?>
         <?php hm_get_template_part('tile', ['bg' => 'grey-bg', 'width' => '1x']) ?>
-       </section>
+      </div>
+    </section>
 
-       </main><!-- /.blog-main -->
+    <? get_sidebar(); ?>
+  </div>
 
-      <? get_sidebar(); ?>
-
-     </div><!-- /.row -->
-  </div><!-- /.container -->
+  </main><!-- /.container -->
   <section class="full-module newsletter-signup">
     <div class="wrapper container-fluid">
         <div class="row">
--- wp-content/themes/vivekbedi/news.php
+++ wp-content/themes/vivekbedi/news.php
@@ -2,20 +2,20 @@
 
 <?php get_header(); ?>
  <div class="">
-  <section class="hero news row">
+  <section class="hero newspage row">
     <h1 class="hero__title"><?php  ?></p>
   </section>
 </div>
 <main class="wrapper container-fluid">
   <div class="row">
-    <div class="col-xs-12">
-      <div class="module-header row">
-        <h2 class="module-title"> News</h2>
-        <a class="module-link down-1 col-xs-4" href="">See All News</a>
+    <div class="col-xs-12 main">
+      <div class="module-header">
+        <h2 class="module-title">News</h2>
+
       </div>
     </div>
   </div>
-  <section class="tile-container">
+  <section class="tile-container tile-3up">
     <div class="row">
       <?php hm_get_template_part('tile', ['bg' => 'dark-bg', 'width' => '1x']) ?>
       <?php hm_get_template_part('tile', ['bg' => 'dark-bg', 'width' => '1x']) ?>
