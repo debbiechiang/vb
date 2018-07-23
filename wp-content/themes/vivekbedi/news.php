36df219d697ab90658862ef5d0b662e220d9b522
debbiechiang <971618+debbiechiang@users.noreply.github.com>
2018-07-23 03:23:37 -0400

    News page hooked up to real posts.

--- wp-content/themes/vivekbedi/news.php
+++ wp-content/themes/vivekbedi/news.php
@@ -11,26 +11,38 @@
     <div class="col-xs-12 main">
       <div class="module-header">
         <h2 class="module-title">News</h2>
-
       </div>
     </div>
   </div>
   <section class="tile-container tile-3up">
-    <div class="row">
-      <?php hm_get_template_part('tile', ['bg' => 'dark-bg', 'width' => '1x']) ?>
-      <?php hm_get_template_part('tile', ['bg' => 'grey-bg', 'width' => '1x']) ?>
-      <?php hm_get_template_part('tile', ['bg' => 'dark-bg', 'width' => '1x']) ?>
-    </div>
-    <div class="row">
-      <?php hm_get_template_part('tile', ['bg' => 'grey-bg', 'width' => '1x']) ?>
-      <?php hm_get_template_part('tile', ['bg' => 'dark-bg', 'width' => '1x']) ?>
-      <?php hm_get_template_part('tile', ['bg' => 'grey-bg', 'width' => '1x']) ?>
-    </div>
-    <div class="row">
-      <?php hm_get_template_part('tile', ['bg' => 'dark-bg', 'width' => '1x']) ?>
-      <?php hm_get_template_part('tile', ['bg' => 'grey-bg', 'width' => '1x']) ?>
-      <?php hm_get_template_part('tile', ['bg' => 'dark-bg', 'width' => '1x']) ?>
-    </div>
+    <?php 
+      $args =  array( 
+        'post_type' => 'post',
+        'orderby' => 'date',
+        'order' => 'DESC', 
+        'posts_per_page' => '9'
+      );
+      $postitem = 0;
+      $custom_query = new WP_Query( $args );
+      while($custom_query->have_posts()) : $custom_query->the_post();?>
+        <?php if($postitem%3 == 0): ?>
+          <div class="row">
+        <?php endif ?>
+        <article class="tile tile__1x tile--<?php if($postitem%2 == 0) { echo "dark-bg";} else {echo "grey-bg"; } ?>">
+            <div class="tile__meta"> 
+              <div class="tile__category down-1"><?php the_category(' ') ?></div>
+              <div class="tile__date down-1"><?php the_date() ?></div>
+            </div>
+            <h3 class="tile__title"><a href=""><?php the_title() ?></a></h3>
+            <div class="tile__tags"><?php the_tags('<ul><li class="tile__tag"><a href="" class="tile__tag-link down-1">', '</a></li><li class="tile__tag"><a href="" class="tile__tag-link down-1">', '</a></li></ul>'); ?>
+            </div>
+        </article>
+
+        <?php if($postitem%3 == 2): ?>
+          </div>
+        <?php endif ?>
+
+    <?php $postitem++; endwhile; ?>
   </section>
 </main><!-- /.container -->
   <?php get_template_part('newsletter') ?>
