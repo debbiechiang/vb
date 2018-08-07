e0254391428688bfaf77d50fe6c27a91b115bd31
debbiechiang <971618+debbiechiang@users.noreply.github.com>
2018-08-07 02:43:05 -0400

    Dropdowns.

--- wp-content/themes/vivekbedi/news.php
+++ wp-content/themes/vivekbedi/news.php
@@ -32,6 +32,62 @@
             </div>
           </div>
         </div>
+
+          <form id="tag-select" class="filter-dropdown tag-select" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
+
+            <?php
+            $args = array(
+              'show_option_none' => __( 'Tags' ),
+              'show_count'       => 1,
+              'orderby'          => 'name',
+              'taxonomy'         => 'post_tag',
+              'echo'             => 0,
+            );
+            ?>
+
+            <?php $select  = wp_dropdown_categories( $args ); ?>
+            <?php $replace = "<select$1 onchange='return this.form.submit()'>"; ?>
+            <?php $select  = preg_replace( '#<select([^>]*)>#', $replace, $select ); ?>
+
+            <?php echo $select; ?>
+
+            <noscript>
+              <input type="submit" value="View" />
+            </noscript>
+
+          </form>
+
+          <form id="category-select" class="filter-dropdown category-select" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
+
+            <?php
+            $args = array(
+              'show_option_none' => __( 'Content Type' ),
+              'show_count'       => 1,
+              'orderby'          => 'name',
+              'echo'             => 0,
+            );
+            ?>
+
+            <?php $select  = wp_dropdown_categories( $args ); ?>
+            <?php $replace = "<select$1 onchange='return this.form.submit()'>"; ?>
+            <?php $select  = preg_replace( '#<select([^>]*)>#', $replace, $select ); ?>
+
+            <?php echo $select; ?>
+
+            <noscript>
+              <input type="submit" value="View" />
+            </noscript>
+
+          </form>
+
+          <form id="date-select" class="filter-dropdown date-select">
+            <select name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
+              <option value=""><?php echo esc_attr( __( 'Date' ) ); ?></option> 
+              <?php wp_get_archives( array( 'type' => 'monthly', 'format' => 'option', 'show_post_count' => 1 ) ); ?>
+            </select>
+        </form>
+
+
         <section class="tile-container tile-3up">
         <?php endif; ?>
 
